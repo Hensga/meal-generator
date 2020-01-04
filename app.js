2541618e652a6fb1f6fd9e20da92f821492c1c84
Henning Huth <henzgarthebarbar@gmail.com>
2020-01-04 22:22:00 +0100

    Schleife erstellt um durch alle im Array vorhandenen Zutaten zu gehen. Erster versuch Informationen aus der DB auf der Seite anzuzeigen mit dem Essens Thumbnail

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 document.getElementById('getMeal').addEventListener('click', getMeal);
+const essen = document.getElementById('meal');
 
 function getMeal() {
+  // Längerer weg (älterer) um an die DB zu kommen
   //   fetch('https://www.themealdb.com/api/json/v1/1/random.php')
   //     .then(function(res) {
   //       return res.json();
@@ -12,9 +14,28 @@ function getMeal() {
   fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(res => res.json())
     .then(data => {
+      essenErstellen(data.meals[0]);
+      console.log(meal);
       console.log(data);
     })
     .catch(e => {
-      console.log(e);
+      console.warn(e);
     });
 }
+
+const essenErstellen = meal => {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+  const newInnerHTML = `
+  <img src="${meal.strMealThumb}" alt="Meal Image">
+  `;
+  essen.innerHTML = newInnerHTML;
+};
